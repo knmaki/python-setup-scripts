4a551bb2c24cfce93b73e4f8d7a17cc71f94f1da
knmaki <knmaki>
2023-10-15 13:55:52 +0900

    * bug fix: ./Python/settings/installation_settings.jsonで指定したPythonバージョンのPythonパッケージをダウンロードするよう修正。

--- Python/scripts/download.ps1
+++ Python/scripts/download.ps1
@@ -1,12 +1,12 @@
 #以下のコマンドでダウンロードする
 #Set-ExecutionPolicy Bypass -Scope process
 
-# TODO: platform、python-versionを指定するとバイナリーのダウンロードしかできない
-# この場合、xlwingsなど、ソース（xlwings本体）とバイナリ（pywin32）が混ざっていると
-# ソースがダウンロードできないのでエラーとなり、--destに（バイナリーだけ出力して欲しいが）
-# 何も出力されない。依存関係のバイナリー パッケージをrequirements.txtで個別に指定すると
-# 依存関係に依っては複雑になり過ぎるため、行っていない。
+$settings = Join-Path $PSScriptRoot '../settings/installation_settings.json' | Resolve-Path | Get-Content -Encoding UTF8 -Raw | ConvertFrom-Json
+$settings.installer -match 'python-\d.\d+'
+$pythonVersion = ($Matches[0] -Replace "python-", "") -Replace "\.", ""
+$pythonExe = Join-Path $env:PROGRAMFILES "/Python${pythonVersion}/python.exe"
 
 $packages = (Join-Path $PSScriptRoot '..\packages')
 $requirements = (Join-Path $PSScriptRoot '..\settings\requirements.txt')
-python -m pip download --dest=$packages --requirement $requirements
+
+& $pythonExe -m pip download --dest=$packages --requirement $requirements
